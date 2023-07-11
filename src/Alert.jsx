8abefefa5155922802a47ccbc3cc56711812319f
HimanshuSinghSatyal <113695312+HimanshuSinghSatyal@users.noreply.github.com>
2023-07-11 13:29:58 +0000

    New change

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -1,6 +1,6 @@
-git remote add origin https://github.com/HimanshuSinghSatyal/mycrat.git
-git branch -M main
-git push -u origin maiimport React,{useEffect} from "react";
+// git remote add origin https://github.com/HimanshuSinghSatyal/mycrat.git
+//git branch -M main
+//git push -u origin maiimport React,{useEffect} from "react";
 import {IoMdCheckbox } from "react-icons/io";
 import  {CgDanger} from "react-icons/cg";
 import {alertContext} from "./Context";
